0dc202fe67de7716ab4acb812b02e7aea003d9ea
Antsuhue <anderson_julio_15@hotmail.com>
2020-06-06 12:08:17 -0300

    testes

--- tikos/extensions/views.py
+++ tikos/extensions/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 from ..functions import products
 
 bp = Blueprint("bp", __name__, static_folder="static", template_folder="template")
 
 def init_app(app):
     app.register_blueprint(bp)
+
+@bp.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
 
 @bp.route("/product", methods=["POST", "GET"])
 def product():
@@ -17,3 +21,11 @@ def find_product(name_product):
 @bp.route("/product/add_qntd/<name_product>", methods=["PUT"])
 def add_qnt_produtcs(name_product):
     return products.add_product(name_product.lower())
+
+@bp.route("/product/withdraw_qntd/<name_product>", methods=["PUT"])
+def withdraw_qnt_produtcs(name_product):
+    return products.withdraw_product(name_product.lower())
+
+@bp.route("/pdf")
+def pdf():
+    return products.generate_pdf_products()
